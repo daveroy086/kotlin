cf977af9f0426016cc7a974d12783e1062a8d5af
daveroy086 <51098204+daveroy086@users.noreply.github.com>
2023-03-07 10:25:44 -0500

    used when and for

---
+++ cw8/twoFortyTwo.kt
@@ -0,0 +1,17 @@
+// twoFortyTwo.kt
+
+fun twoFortyTwo(games: List<String>): Int {
+
+        var result: Int = 0
+        for(score in games) {
+            when{
+                score[0] > score[2] -> result += 3
+                score[0] == score[2] ->     result++
+            }
+        }
+
+    return result
+}
+// end 242 
+
+// twoFortyTwo(listOf("1:1", "2:2", "3:3", "4:4", "2:2", "3:3", "4:4", "3:3", "4:4", "4:4"))    // 10
